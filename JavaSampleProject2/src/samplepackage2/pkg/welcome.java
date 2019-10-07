004f36f2561247f6ba1609aa07355f36017db767
krishnapriya756 <rk.krishnapriya@gmail.com>
2019-10-07 19:03:41 -0400

    javaproject2 to github

---
+++ JavaSampleProject2/src/samplepackage2/pkg/welcome.java
@@ -0,0 +1,11 @@
+package samplepackage2.pkg;
+
+public class welcome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Project2");
+
+	}
+
+}
